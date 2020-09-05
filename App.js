f2a783adec701534915f90e1f9dcbe18c243a5b5
andersonfebs <andersonfebs@gmail.com>
2020-09-05 18:28:26 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,173 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Constants from 'expo-constants';
+
+import React, { useEffect, useState } from 'react';
+import { Alert, AsyncStorage, Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [anotation, setAnotation] = useState('');
+  const [state, setState] = useState('read');
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const savedAnnotation = await AsyncStorage.getItem('key');
+        setAnotation(savedAnnotation);
+        console.log(savedAnnotation);
+      } catch {
+
+      }
+    })();
+
+
+  }, []);
+
+   async function saveData() {
+    try {
+      await AsyncStorage.setItem('key', anotation);
+    } catch (error) {
+      
+    }
+
+    Alert.alert('Anotação salva com sucesso!');
+  }
+
+  function updateText() {
+    setState('read');
+    saveData();
+  }
+
+
+  if (state == 'read') {
+    return (
+      <View style={{flex: 1, marginTop: Constants.statusBarHeight}}> 
+        <StatusBar style="dark" />
+        <View style={styles.barTitle}>
+          <Text style={styles.barTitleText}>App Anotações</Text>
+        </View>
+        <View style={styles.body}>
+          {
+            (anotation != '') ?
+              <Text style={styles.bodyText}>{anotation}</Text>
+            :
+              <Text style={styles.bodyTextNoText}>Não há anotações no momento :(</Text>            
+          }
+        </View>         
+          <TouchableOpacity
+            onPress={() => setState('write')}
+            style={styles.button}>
+              {
+                (anotation == '') ?
+                <Text style={styles.buttonText}>
+                  +
+                </Text>
+                :
+                <Text style={styles.buttonTextEdit}>
+                  Editar
+                </Text>
+              }
+          </TouchableOpacity>
+      </View>
+    );
+  } else if (state == 'write') {
+    return (
+      <View style={{flex: 1, marginTop: Constants.statusBarHeight}}> 
+        <StatusBar style="dark" />
+        <View style={styles.barTitle}>
+          <Text style={styles.barTitleText}>App Anotações</Text>
+        </View>
+        <View style={styles.body}>
+          
+          <TextInput 
+            style={styles.bodyText}
+            autoFocus={true}
+            value={anotation}  
+            multiline={true}
+            numberOfLines={5}
+            onChangeText={(text) => setAnotation(text)}
+          />
+        </View>
+        <TouchableOpacity 
+          onPress={updateText} 
+          style={styles.buttonSave}>
+            <Text style={styles.buttonSaveText}>Salvar</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+  
+
+  
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  barTitle: {
+    backgroundColor: 'blue',
+    padding: 20,
+  },
+
+  barTitleText: {
+    color: 'white',
+    fontSize: 25,
+    textAlign: "center",
+    fontWeight: "bold"
+  },
+
+  body: {
+    padding:20
+  },
+
+  bodyText: {
+    textAlign: 'justify',
+    fontSize: 20,
+    fontStyle: "normal",
+    textAlignVertical: "top"
+  },
+
+  bodyTextNoText: {
+    textAlign: 'justify',
+    fontSize: 20,
+    fontStyle: "normal",
+    textAlignVertical: "top",
+    opacity: 0.3
+  },
+
+  button: {
+    position: 'absolute',
     alignItems: 'center',
     justifyContent: 'center',
+    right: 20,
+    bottom: 20,
+    backgroundColor: 'blue',
+    height: 50,
+    width: 50,
+    borderRadius: 25
   },
-});
+
+  buttonText: {
+    color: 'white',
+    fontSize: 25
+  },
+
+  buttonTextEdit: {
+    color: 'white',
+    fontSize: 15
+  },
+
+  buttonSave: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    bottom: 0,
+    backgroundColor: 'blue',
+    height: 50,
+    width: '100%'
+  },
+
+  buttonSaveText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: "normal"
+  }
+})
---
+++ README.md
@@ -0,0 +1,41 @@
+<h1 align="center">
+    <img src="assets/screens/Screenshot_1599327426.png">
+    <img src="assets/screens/Screenshot_1599327550.png">
+</h1>
+
+## 🐱‍💻 Sobre
+
+Esta é uma aplicação simples desenvolvida a partir do **Curso de Desenvolvimento de Apps** da Danki Code. O app é um aplicativo de anotação simples que permite a leitura e escrita de anotações. Toda anotação é salva na **memória do dispositivo (storage)**, permitindo que o app seja fechado e aberto, posteriormente, sem que haja perda de informação.
+
+---
+
+## ✔ Tecnologias utilizadas
+
+- React Native
+- Expo
+- Javascript
+- NodeJS
+- Yarn
+
+---
+
+## ✈ Como baixar o projeto
+
+```bash
+
+    # Clonar o repositório
+    $ git clone https://github.com/souza-anderson/nlw-ecoleta
+
+    # Entrar no diretório do projeto
+    $ cd app-notes
+
+    # Instalar as dependências
+    $ yarn
+
+    # Executar o projeto
+    $ yarn start
+
+```
+---
+## :page_facing_up: Licença
+Desenvolvido por Anderson Souza e sob a licença [MIT](/LICENSE).
